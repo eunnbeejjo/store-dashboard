90e82e11e286603612b5d7ced32fbf89b501278f
조은비 <eunnbeejjo@jo-eunbiui-MacBookAir.local>
2025-12-11 07:10:01 +0900

    refactor: 상세 페이지 코드정리
    * 리팩토링

--- apps/frontend/src/pages/Detail/DetailPage.tsx
+++ apps/frontend/src/pages/Detail/DetailPage.tsx
@@ -9,7 +9,6 @@ import { Link, useParams } from 'react-router-dom';
 function DetailPage() {
   const { id } = useParams();
   const { info } = useStore();
-
   const { data, isLoading, isError, error } = useCustomerPurchases(Number(id));
 
   useEffect(() => {
@@ -17,9 +16,6 @@ function DetailPage() {
       alert(error.message);
     }
   }, [isError, error]);
-
-  if (isLoading) return <div>로딩 중...</div>;
-  if (!data || data.length === 0) return <div>데이터 없음</div>;
 
   const headers: DTableHeader[] = [
     { key: 'date', name: '구매일' },
@@ -43,12 +39,13 @@ function DetailPage() {
     [data],
   );
 
+  if (isLoading) return <div>로딩 중...</div>;
+  if (!data || data.length === 0) return <div>데이터 없음</div>;
+
   return (
     <div className={s.DetailPage}>
       <h2 className={s.Title}>{info?.name}의 구매내역</h2>
-
       <Table headers={headers} items={rows} />
-
       <div className="flex gap-x-5">
         <Link to="/">메인페이지로 이동</Link>
         <Link to="/list">고객 리스트로 이동</Link>
